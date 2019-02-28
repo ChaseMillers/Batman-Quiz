ed121932439d28457d85e7966c36a8b690b32f85
ChaseMillers <46936509+ChaseMillers@users.noreply.github.com>
2019-02-27 20:54:12 -0800

    further quality tweaking

--- index.js
+++ index.js
@@ -1,119 +1,121 @@
-let questionNumber = 0;
 let score = 0;
+let questionNumber = 0;
+//MAIN Functions
+//Quiz Start//Generate question//sellect answer//user feedback for correct and wrong// finall page
+//SUPORTING Functions
+//Add to score//Add question//Next button//Restartbutton//Submit button//Run functions//
+//Global Vars: score, question number
 
-// MAKE THE QUESTION
-function generateQuestion () {
-  if (questionNumber < STORE.length) {
-    $('.questionAnswerForm').html (`<div class="question">
-    <h2>${STORE[questionNumber].question}</h2>
+//Note: the value of questionNumber is set to 0 only for the array. By default the text will display at 1;
+
+function startQuiz (){
+  $('.startButton').on('click', (event)=>{
+  $('.quizStart').css('display','none');
+  $('.questionForm').css('display','block');
+  $('footer ul').css('display','block');
+  createQuestion();
+  selectAnswer()
+  });
+}
+
+//Go into object QUESTIONS, grab [array number] and sellect .question to display in h2
+//value uses' infromation stored for that answer, the span holds the text 
+function createQuestion(){
+  if (questionNumber<QUESTIONS.length){
+    $('.questionForm').html(`<div class='question'>
+    <h2>${QUESTIONS[questionNumber].question}</h2>
     <form>
     <fieldset>
-    <label class="answerOption">
-    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
-    <span>${STORE[questionNumber].answers[0]}</span>
+    <label class='answerOption'>
+    <input type='radio' value='${QUESTIONS[questionNumber].answers[0]}' name='answer' required>
+    <span>${QUESTIONS[questionNumber].answers[0]}</span>
     </label>
-    <label class="answerOption">
-    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
-    <span>${STORE[questionNumber].answers[1]}</span>
+    <label class='answerOption'>
+    <input type='radio' value='${QUESTIONS[questionNumber].answers[1]}' name='answer' required>
+    <span>${QUESTIONS[questionNumber].answers[1]}</span>
     </label>
-    <label class="answerOption">
-    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
-    <span>${STORE[questionNumber].answers[2]}</span>
+    <label class='answerOption'>
+    <input type='radio' value='${QUESTIONS[questionNumber].answers[2]}'' name='answer' required>
+    <span>${QUESTIONS[questionNumber].answers[2]}</span>
     </label>
-    <button type="submit" class="submitButton">Submit</button>
+    <button type='submit' class='submitButton'>Submit</button>
     </fieldset>
     </form>
     </div>`);
 } else {
-    renderResults();
-    restartQuiz();
+    results();
+    resetPage();
     $('.questionNumber').text(10)
   }
 }
 
-//increment score
-function changeScore () {
-  // tells score to add 1
-  score ++;
-  // updates actuall text 
-  $('.score').text(score);
-}
-
-//Quiz starts// removes .quizStart// turns on .questionAnswerForm and footer// starts off at question number 1
-function startQuiz () {
-  $('.quizStart').on('click', '.startButton', (event) =>{
-    $('.quizStart').remove();
-    $('.questionAnswerForm').css('display', 'block');
-    $('footer ul').css('display', 'block');
-    $('.questionNumber').text(1);
-});
-}
-
-//user selects answer on submit run user feedback
-function userSelectAnswer () {
-  $('form').on('submit', (event) => {
+function selectAnswer(){
+  $('form').on('submit',(event)=>{
     event.preventDefault();
-    let selected = $('input:checked');
-    let answer = selected.val();
-    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    let answer = $('input:checked').val();
+    let correctAnswer = `${QUESTIONS[questionNumber].correctAnswer}`;
     if (answer === correctAnswer) {
-      userAnswerFeedbackCorrect();
-      changeScore();
-    } else {
-      userAnswerFeedbackWrong();
+      //add to array then text
+      score ++;
+      $('.score').text(score);
+      displayCorrect();
+    }
+    else {
+      displayWrong();
     }
   });
 }
 
-//user feedback for correct answer
-function userAnswerFeedbackCorrect () {
-  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
-  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><p><b>CORRECT!</b></p><img src="${STORE[questionNumber].icon}" alt="${STORE[questionNumber].alt}"/></div><p><span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
+//html will delete previous text with new
+function displayCorrect(){
+let correctAnswer = `${QUESTIONS[questionNumber].correctAnswer}`;
+$('.questionForm').html(`<div class="correctFeedback"><div class="icon"><p><b>CORRECT!</b></p><img src="${QUESTIONS[questionNumber].giff}" alt="${QUESTIONS[questionNumber].alt}"/></div><p><span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
 }
 
-//user feedback for wrong answer
-function userAnswerFeedbackWrong () {
-  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
-  // let iconImage = `${STORE[questionNumber].icon}`;
-  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><p><b>WRONG!</b></p><img src="${STORE[questionNumber].icon}" alt="${STORE[questionNumber].alt}"/></div><p>The correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
+function displayWrong(){
+let correctAnswer = `${QUESTIONS[questionNumber].correctAnswer}`;
+$('.questionForm').html(`<div class="correctFeedback"><div class="icon"><p><b>Wrong!</b></p><img src="${QUESTIONS[questionNumber].giff}" alt="${QUESTIONS[questionNumber].alt}"/></div><p>The corect answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
 }
 
-//when quiz is over this is the html for the page
-function renderResults () {
-  if (score >= 8) {
-    $('.questionAnswerForm').html(`<div class="correctFeedback"><h3>Phenomenal work! </h3><img src="https://media.giphy.com/media/3z4Wju1DmiRoI/giphy.gif" alt="Batman Eating"/><p>You got ${score} /10</p><p>May your friends and Mom proudly call you Batman!</p><button class="restartButton">Restart Quiz</button></div>`);
-  } else if (score < 8 && score >= 5) {
-    $('.questionAnswerForm').html(`<div class="correctFeedback"><h3>I dont know about Batman</h3><img src="https://media.giphy.com/media/MoBklgH66BKxi/giphy.gif" alt="Robin is Wowed"/><p>You got ${score} /10</p><p>But you sure would make a good Robin!</p><button class="restartButton">Restart Quiz</button></div>`);
+function nextQuestion(){
+  $('main').on('click','.nextButton',(event)=>{
+    //represents array value
+    questionNumber ++;
+    //represents text value which is 1 behind because it started at 0
+    $('.questionNumber').text(questionNumber+1);
+    createQuestion();
+    selectAnswer();
+  });
+}
+
+function results(){
+if (score >=8){
+   $('.questionForm').html(`<div class="correctFeedback"><h3>Phenomenal work! </h3><img src="https://media.giphy.com/media/3z4Wju1DmiRoI/giphy.gif" alt="Batman Eating"/><p>You got ${score} /10</p><p>May your friends and Mom proudly call you Batman!</p><button class="restartButton">Restart Quiz</button></div>`);
+  } else if (score<8 && score>=5){
+  $('.questionForm').html(`<div class="correctFeedback"><h3>I dont know about Batman</h3><img src="https://media.giphy.com/media/MoBklgH66BKxi/giphy.gif" alt="Robin is Wowed"/><p>You got ${score} /10</p><p>But you sure would make a good Robin!</p><button class="restartButton">Restart Quiz</button></div>`);
   } else {
-    $('.questionAnswerForm').html(`<div class=" correctFeedback"><h3>Yikes!</h3><img src="https://media.giphy.com/media/fHxgDPtfCpd5u/giphy.gif" alt="Batman shooting gun"/><p>You only got ${score} /10</p><p>I think you need help.</p><button class="restartButton">Restart Quiz</button></div>`);
+  $('.questionForm').html(`<div class=" correctFeedback"><h3>Yikes!</h3><img src="https://media.giphy.com/media/fHxgDPtfCpd5u/giphy.gif" alt="Batman shooting gun"/><p>You only got ${score} /10</p><p>I think you need help.</p><button class="restartButton">Restart Quiz</button></div>`);
   }
 }
-
-//what happens when the user clicks next
-function renderNextQuestion () {
-  $('main').on('click', '.nextButton', (event)=>{
-    // reprents actuall value
-    questionNumber ++;
-    // represents text
-    $('.questionNumber').text(questionNumber+1);
-    generateQuestion();
-    userSelectAnswer();
+// Better then using location.reload();???
+function resetPage(){
+  $('.restartButton').on('click',(event)=>{
+  $('.quizStart').css('display','block');
+  $('.questionForm').css('display','none');
+  $('footer ul').css('display','none');
+  createQuestion();
+  selectAnswer()
+  score = 0;
+  $('.score').text(0);
+  questionNumber = 0;
+  $('.questionNumber').text(1);
   });
 }
 
-//restart quiz function - reloads page to start quiz over
-function restartQuiz () {
-  $('main').on('click', '.restartButton', (event)=>{
-    location.reload();
-  });
+// Better of worse to have functions activate at start of page or Manually chained by each other
+// Better to make add point and add question a function?
+function quizApp(){
+$(startQuiz);
+$(nextQuestion);
 }
-
-//run quiz functions
-function createQuiz () {
-  startQuiz();
-  generateQuestion();
-  userSelectAnswer();
-  renderNextQuestion();
-}
-
-$(createQuiz);
+$(quizApp);
